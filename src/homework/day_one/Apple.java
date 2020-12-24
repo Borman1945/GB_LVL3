9dd9982650670df09077de0311338ef2201e8506
moshkabortman <tryagenbutro@yandex.ru>
2020-12-25 01:09:29 +0300

    добавил всю домашку

---
+++ src/homework/day_one/Apple.java
@@ -0,0 +1,13 @@
+package homework.day_one;
+
+public class Apple extends Fruit{
+
+    private static final double appleWeight = 1;
+
+    public Apple() {
+        super(appleWeight);
+    }
+
+
+
+}
